2b14f6f9747b798c2ffd220af9befce8464ba77c
Lavínia Rodrigues <xenia@kernel32.xyz>
2024-09-11 23:05:09 -0300

    add nix flake for development shell

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  
+  outputs = { self, nixpkgs, ... }: let
+    systems = [ "x86_64-linux" "aarch64" ];
+
+    forEachSystem = f: with nixpkgs.lib; foldAttrs mergeAttrs {}
+      (lists.forEach systems (system:
+        mapAttrs (name: value: { ${system} = value; }) (f system)
+      ));
+  in forEachSystem (system: let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in rec {
+    devShells.default = pkgs.mkShell {
+      inputsFrom = [
+        packages.default
+      ];
+    };
+
+    packages.default = pkgs.stdenv.mkDerivation {
+      name  = "kernel32";
+      pname = "kernel32";
+
+      src = ./.;
+
+      nativeBuildInputs = with pkgs; [ go hugo ];
+      
+      buildPhase = "hugo --gc --minify --baseURL https://kernel32.xyz";
+    };
+  });
+}
